0c64b4a5745a3e02b0e4b9edffacd0e8c42cdba2
CallMeKhiloo <khm40005000@outlook.com>
2026-02-22 01:03:25 +0200

    feat: added the authentication needed for login and the authorization middlewares

---
+++ middlewares/auth.js
@@ -0,0 +1,50 @@
+const jwt = require('jsonwebtoken');
+const Users = require('../models/userModel');
+
+const protect = async (req, res, next) => {
+  // 401 -> unauthorized
+  try {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token)
+      throw new Error('You are not logged in! Please log in to get access.');
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    const currentUser = await Users.findById(payload.userId);
+    if (!currentUser)
+      throw new Error('The user belonging to this token no longer exists.');
+
+    req.user = currentUser;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const restrictTo = (...roles) => {
+  // 403 -> forbidden
+  /* 
+  restrictTo will be executed when server starts,
+  then the returned function will execute whenever this middleware is used
+  when there is a call to this specific route
+  */
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      const error = new Error(
+        'You do not have permission to perform this action',
+      );
+      return next(error);
+    }
+
+    next();
+  };
+};
+
+module.exports = { protect, restrictTo };
